78fe9e97298207aab25df48f287d99972d52d555
Vz0n <65766278+Vz0n@users.noreply.github.com>
2025-06-23 07:57:39 -0400

    remove tangent_plane scene from discord_banner1

--- src/discord_banner1.py
+++ src/discord_banner1.py
@@ -1,6 +1,5 @@
-# This code generates a stupidly simple animation of a paraboloid shaped surface, showing it's tangent lines (1st order partial derivatives) 
-# and the tangent plane to the point (1,1,2).
-# Nothing else.
+# This code generates a stupidly simple animation of a paraboloid surface, showing it's tangent lines (1st order partial derivatives) 
+# and the gradient of the function. Nothing else
 
 from manim import *
 
@@ -10,9 +9,6 @@ TEXT_POSITION = (2.5, 5, 1.5)
 DEFAULT_RANGE = (-25, 25, 1)
 DEFAULT_UV_RANGE = (-25, 25)
 
-def plane(u: float, v:float):
-    return 2*(u+v) - 2
-
 def paraboloid(u:float, v:float) -> float:
     return u**2 + v**2
 
@@ -21,18 +17,18 @@ def partial(u):
     return 2*u
 
 def prepare_text(text: Text|MathTex):
-    return text.scale(0.7).rotate(numpy.pi/2, RIGHT).move_to(TEXT_POSITION).rotate_about_origin(numpy.pi/3, IN)
+    return text.scale(0.8).rotate(numpy.pi/2, RIGHT).move_to(TEXT_POSITION).rotate_about_origin(numpy.pi/3, IN)
 
 class DiscordBanner(ThreeDScene):
     def construct(self):
         coord_system = ThreeDAxes(x_range=DEFAULT_RANGE, y_range=DEFAULT_RANGE, z_range=DEFAULT_RANGE)
-        text = prepare_text(MathTex("z = x^2 + y^2"))        
-        partial_text = lambda var : prepare_text(MathTex(f"\\nabla_{var}z = 2{var}"))
+        text = prepare_text(MathTex("f(x,y) = x^2 + y^2"))
+        partial_text = lambda var : prepare_text(MathTex(f"\\nabla_{var}f = 2{var}"))
 
         par_x = partial_text("x")
         par_y = partial_text("y")
         
-        tsurface_eq = prepare_text(MathTex("2x + 2y - z = 2"))
+        nablaf = prepare_text(MathTex("\\nabla f = 2xe_1 + 2ye_2"))
 
         surface = coord_system.plot_surface(
             paraboloid,
@@ -40,15 +36,6 @@ class DiscordBanner(ThreeDScene):
             v_range=DEFAULT_UV_RANGE,
             resolution=(32, 32),
             colorscale=[PURPLE, BLUE, PURPLE, PURPLE]
-        )
-
-        # A tangent plane to (1, 1, r(1,1))
-        tangent_plane = coord_system.plot_surface(
-            plane,
-            u_range=DEFAULT_UV_RANGE,
-            v_range=DEFAULT_UV_RANGE,
-            resolution=(32, 32),
-            colorscale=[PURPLE, BLUE]
         )
 
         space = numpy.linspace(start=DEFAULT_UV_RANGE[0], stop=DEFAULT_UV_RANGE[1])
@@ -70,9 +57,7 @@ class DiscordBanner(ThreeDScene):
            Create(par_y),
            Wait(0.5),
            FadeOut(par_y),
-           Create(tangent_plane),
-           Rotate(tangent_plane, angle=numpy.pi/2),
-           Create(tsurface_eq),
+           Create(nablaf),
            Wait(1)
         ]
 
